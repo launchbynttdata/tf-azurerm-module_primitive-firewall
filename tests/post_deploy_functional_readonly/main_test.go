96390694f1d8a1719f67fa8ebd734e069b5835e8
Chris Taylor <chris11.taylor@nttdata.com>
2024-05-23 10:57:55 -0500

    Fix readonly test examples folder reference

--- tests/post_deploy_functional_readonly/main_test.go
+++ tests/post_deploy_functional_readonly/main_test.go
@@ -21,7 +21,7 @@ import (
 )
 
 const (
-	testConfigsExamplesFolderDefault = "../../examples/firewall"
+	testConfigsExamplesFolderDefault = "../../examples"
 	infraTFVarFileNameDefault        = "test.tfvars"
 )
 

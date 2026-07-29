2ddec3235edb17175b8de7f9e7b572cfebc64c13
rakesh gorige <rakesh.gorige@resideo.com>
2026-07-29 01:24:15 -0500

    feat!: convert to copier-managed skeleton

--- tests/post_deploy_functional_readonly/main_test.go
+++ tests/post_deploy_functional_readonly/main_test.go
@@ -38,5 +38,5 @@ func TestFirewallModule(t *testing.T) {
 		}).
 		Build()
 
-	lib.RunNonDestructiveTest(t, *ctx, testimpl.TestFirewall)
+	lib.RunNonDestructiveTest(t, *ctx, testimpl.TestComposableFirewall)
 }
